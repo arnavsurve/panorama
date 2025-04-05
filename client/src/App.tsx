7cc4686426bd3479ad2308e2cae2803392d805bf
Syrus Aslam <syrusa663@gmail.com>
2025-04-04 17:44:41 -0700

    init client with ts and broken shadcn

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Textarea } from "@/components/ui/textarea"
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Textarea />
+    </>
+  )
+}
+
+export default App
